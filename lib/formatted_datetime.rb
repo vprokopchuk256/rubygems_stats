a1c1993e6936e07fe45ce769520052f316824417
Valeriy Prokopchuk <vprokopchuk@gmail.com>
2015-12-04 23:34:58 +0200

    simple format

--- lib/formatted_datetime.rb
+++ lib/formatted_datetime.rb
@@ -2,6 +2,19 @@ require_relative 'formatted_value'
 
 class FormattedDatetime < FormattedValue
   def initialize value, format
-    super(format.format.present? ? value.to_datetime.strftime(format.format) : nil, format)
+    super(format.format.present? ? value.to_datetime.strftime(date_format(format.format)) : nil, format)
+  end
+
+  private
+
+  def date_format format
+    case format
+      when 'mins', 'mns', 'min' then '%-d %b %y %H:%M'
+      when 'hour', 'hrs', 'hs', 'h' then '%-d %b %y %H:00'
+      when 'day', 'd', 'days' then '%-d %b %y'
+      when 'months', 'mos', 'm', 'month' then '%b %y'
+      when 'years', 'yrs', 'yr', 'y' then '%Y year'
+      else format
+    end
   end
 end
--- lib/grouper.rb
+++ lib/grouper.rb
@@ -17,8 +17,19 @@ class Grouper
   private
 
   def groups
-    parsed_lines.group_by{|l| l.first.to_datetime.strftime(group_name)}.collect do |_, lines|
+    parsed_lines.group_by{|l| l.first.to_datetime.strftime(group_format)}.collect do |_, lines|
       Group.new(lines)
+    end
+  end
+
+  def group_format
+    case group_name
+      when 'mins', 'mns', 'min' then '%Y%m%d %H:%M'
+      when 'hour', 'hrs', 'hs', 'h' then '%Y%m%d %H'
+      when 'day', 'd', 'days' then '%Y%m%d'
+      when 'months', 'mos', 'm', 'month' then '%Y%m'
+      when 'years', 'yrs', 'yr', 'y' then '%Y'
+      else group_name
     end
   end
 
